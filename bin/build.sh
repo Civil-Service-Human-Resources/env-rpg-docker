ea5213e1f67c2be941fd2d585fc21cc7682a129b
James Kingston <james.kingston@digital.dvla.gov.uk>
2018-05-11 12:07:17 +0100

    change the lookup of script directory

--- bin/build.sh
+++ bin/build.sh
@@ -3,10 +3,13 @@
 # Build images with RPG labels
 # Usage eg ./build.sh java-8-base uid
 
+bin_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+docker_dir="$(dirname ${bin_dir})"
+
 export docker_image=cshrrpg.azurecr.io/${1}
 export docker_tag=${2}
 
 docker build --tag ${docker_image}:${docker_tag} \
 	--build-arg arg_build_branch=$(git rev-parse --abbrev-ref HEAD) \
 	--build-arg arg_build_project=env-rpg-docker \
-	--build-arg arg_built_by=$(whoami) ../${1}/
+	--build-arg arg_built_by=$(whoami) ${docker_dir}/${1}/
