abab8bc79134c903eaa92ba0e7e273f6680add9b
James Kingston <james.kingston@i32.co.uk>
2018-05-14 11:54:34 +0100

    Addition of build date to images

--- bin/build.sh
+++ bin/build.sh
@@ -10,6 +10,7 @@ export docker_image=cshrrpg.azurecr.io/${1}
 export docker_tag=${2}
 
 docker build --tag ${docker_image}:${docker_tag} \
+    --build-arg arg_build_date=$(date +"%Y%m%d_%H-%M_%S") \
 	--build-arg arg_build_branch=$(git rev-parse --abbrev-ref HEAD) \
 	--build-arg arg_build_project=env-rpg-docker \
 	--build-arg arg_built_by=$(whoami) ${docker_dir}/${1}/
